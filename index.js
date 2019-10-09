e3f1ebac6044a303cfedffd4895dcb25c3a2d894
Bartek <taran06.xd@gmail.com>
2019-10-09 19:22:32 +0200

    add first scripts, and text-align: center to article

---
+++ index.js
@@ -0,0 +1,22 @@
+const aboutMe = document.querySelector('.aboutMe');
+const w = document.querySelector('.w');
+const i = document.querySelector('.i');
+
+const articleH1 = document.querySelector('article h1');
+const articleContent = document.querySelector('article p');
+
+
+
+
+aboutMe.addEventListener('click', function(){
+    articleH1.textContent = 'O mnie';
+    articleContent.textContent = 'Nazywam się Michał';
+})
+w.addEventListener('click', function(){
+    articleH1.textContent = 'Gdzie nagrywam?';
+    articleContent.textContent = 'Nagrywam na YT (https://www.youtube.com/channel/UCvWiFXNYoeMUibdIGiNqBIQ?disable_polymer=true)';
+})
+i.addEventListener('click', function(){
+    articleH1.textContent = 'Czym się interesuję';
+    articleContent.textContent = 'Interesuję się słodziakami, lokomotywami, duplo i lego';
+})
